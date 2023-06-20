218cfefe7a472b8970192626a43d1954bbe64ad8
tresor <tshims79@gmail.com>
2023-06-20 20:16:22 +0200

    Created a data folder with the contact API DB contect

---
+++ ContactAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}

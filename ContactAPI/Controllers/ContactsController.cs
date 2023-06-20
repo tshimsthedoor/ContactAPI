5e25631a14f8ef439b70a2485bc747ce4608dd82
tresor <tshims79@gmail.com>
2023-06-20 20:21:29 +0200

    Created my contact controller api class file

---
+++ ContactAPI/Controllers/ContactsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller")]
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
